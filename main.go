0efccf939934ba935c73a6cf59fa45d92191b10c
Lance Feagan <lfeagan@hotmail.com>
2017-12-27 14:57:11 +0800

    add support for flags via viper

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"flag"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"strconv"
+	"fmt"
 )
 
 func main() {
-	hostName := "localhost"
-	hostPort := "8080"
-	listenAddr := hostName + ":" + hostPort
+	flag.String("hostName", "", "the interface address to bind to for listening")
+	flag.Int("hostPort", 8080, "the port to listen on for connections")
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	hostName := viper.GetString("hostName") // retrieve value from viper
+	hostPort := viper.GetInt("hostPort")
+
+	listenAddr := hostName + ":" + strconv.Itoa(hostPort)
+	fmt.Printf("listening on %s\n", listenAddr)
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(listenAddr, router))
 }
